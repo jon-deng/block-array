a89985266faeacc75cda39bb1a56592ef32ae925
Jon Deng <jonathan.j.deng@gmail.com>
2022-01-30 16:02:56 -0500

    Add basic BlockMat tests and a Block matric-vector multiplication

--- blocklinalg/linalg.py
+++ blocklinalg/linalg.py
@@ -8,3 +8,34 @@ from collections import OrderedDict
 import numpy as np
 from petsc4py import PETSc
 
+from .vec import BlockVec
+
+def generic_mult_mat_vec(A, x):
+    """
+    Return a generic matrix vector multiplication
+    """
+    if isinstance(A, PETSc.Mat):
+        y = A.getVecLeft()
+        return A.matMult(x, y)
+    else:
+        raise NotImplementedError("")
+
+def mult_mat_vec(A, x):
+    y_vecs = []
+    for m_row, x_vec in enumerate(x.vecs):
+        y_vec = generic_mult_mat_vec(A.mats[m_row][0], x_vec)
+        for n in range(1, len(A.col_keys)):
+            y_vec += generic_mult_mat_vec(A.mats[m_row][n], x_vec)
+        y_vecs.append(y_vec)
+    return BlockVec(y_vecs, x.keys)
+
+# def mult_mat_mat(A, B):
+#     A_v = []
+#     for m_row, xvec in enumerate(x.vecs):
+#         yvec = generic_mult_mat_mat(A.mats[m_row][0], vec)
+#         for n in range(1, len(A.col_keys)):
+#             yvec += generic_mult_mat_vec(A.mats[m_row][n], vec)
+#         yvecs.append(yvec)
+#     return BlockVec(yvecs, x.keys)
+
+# def mult_mat_mat(A, B):
---
+++ test/test_mat.py
@@ -0,0 +1,63 @@
+import numpy as np
+import petsc4py.PETSc as PETSc
+
+from blocklinalg import mat as bmat
+
+COMM = PETSc.COMM_WORLD
+A = PETSc.Mat().create(COMM)
+A.setSizes([3, 3])
+# A.setPreallocationNNZ(np.array([2, 1, 1], dtype=np.int32)[None, :])
+o_nnz = np.array([2, 1, 1], dtype=np.int32)
+d_nnz = np.zeros(3, dtype=np.int32)
+# A.setPreallocationNNZ((o_nnz, d_nnz))
+# A.setPreallocationNNZ([2, 5, 5])
+# A.setPreallocationNNZ(1)
+A.setType('aij')
+A.setUp()
+# print(A.nnz())
+# print(A.getInfo())
+
+print(A.getOwnershipRange())
+for nrow in range(A.getSize()[0]):
+    rows = np.array([nrow], dtype=np.int32)
+    cols = np.array([0, 1], dtype=np.int32)
+    vals = np.array([1.2, 2.4])
+    A.setValues(rows, cols, vals)
+    
+A.assemble()
+# A.assemblyBegin()
+# A.assemblyEnd()
+# print(A.getInfo())
+print(A[:, :])
+
+B = PETSc.Mat().create(COMM)
+B.setSizes([3, 2])
+B.setType('aij')
+B.setUp()
+B.setValues([0], [0], [5])
+B.assemble()
+print(B[:, :])
+
+C = PETSc.Mat().create(COMM)
+C.setSizes([2, 3])
+C.setUp()
+C.setValues([0], [0], [5])
+C.assemble()
+
+D = PETSc.Mat().create(COMM)
+D.setSizes([2, 2])
+D.setUp()
+D.setValues([0], [0], [2.0])
+D.assemble()
+
+MATS = \
+    [[A, B],
+     [C, D]]
+
+BMAT1 = bmat.BlockMat(MATS)
+BMAT2 = bmat.BlockMat(MATS)
+BMAT3 = BMAT1+BMAT2
+
+print(BMAT1.to_petsc()[:, :])
+print(BMAT3.to_petsc()[:, :])
+
