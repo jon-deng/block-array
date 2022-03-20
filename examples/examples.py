c72fce801b0b6641d97e743d08e2f2371a8b591e
Jonathan Deng <jonathan.j.deng@gmail.com>
2022-03-20 16:50:54 -0400

    Add basic example usage to examples.py

--- examples/examples.py
+++ examples/examples.py
@@ -1,3 +1,43 @@
 """
 A collection of examples illustrating some use cases for blocktensor
 """
+
+import numpy as np
+
+from blocktensor import mat as bmat, vec as bvec
+from blocktensor import linalg as bla
+
+## Storing block matrices
+A00 = np.array(
+    [[1, 2],
+     [4, 5]]
+)
+A01 = np.array(
+    [[3],
+     [6]]
+)
+A10 = np.array(
+    [[7, 8],
+     [3, 4]]
+)
+A11 = np.array(
+    [[9]]
+)
+
+A = bmat.BlockMat(
+    [[A00, A01],
+     [A10, A11]],
+    labels=(('a', 'b'), ('a', 'b')))
+
+B = A.copy()
+
+x0 = np.array([1, 2])
+x1 = np.array([3])
+x = bvec.BlockVec([x0, x1], labels=(('a', 'b'),))
+
+## Basic math operation on matrices
+C = A+B
+C = 2*A
+
+## Basic tensor operations
+y = bla.mult_mat_vec(C, x)
