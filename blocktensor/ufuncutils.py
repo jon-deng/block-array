d8127e2986195c217efe1df29b7460e1b3fc6cb3
Jonathan Deng <jonathan.j.deng@gmail.com>
2022-04-24 22:39:01 -0400

    Add function to interpret ufunc signature

--- blocktensor/ufuncutils.py
+++ blocktensor/ufuncutils.py
@@ -36,11 +36,36 @@ def parse_ufunc_signature(
     sig_outputs = [tuple(sig_output.split(',')) for sig_output in sig_outputs]
     return sig_inputs, sig_outputs
 
-def interpret_ufunc_signature(input_sigs, output_sig):
+def interpret_ufunc_signature(sig_inputs, sig_outputs):
     """
     Interprets a ufunc signature
-
-
     """
+    # TODO: Will have to handle weird signatures where output dimension names
+    # do not match and of the input dimension names
 
-    label_to_type = {label: free}
+    # Get the set of free dimension names and contract (cont) dimension names
+    free_names = {name for sig_output in sig_outputs for name in sig_output}
+    cnrt_names = {
+        name for sig_input in sig_inputs for name in sig_input
+        if name not in free_names
+    }
+    
+    # For each free dimension name, record the input number and axis number that
+    # it occurs in
+    free_name_to_input = {
+        name: (ii_input, ii_ax)
+        for ii_input, sig_input in enumerate(sig_inputs)
+        for ii_ax, name in enumerate(sig_input)
+        if name in free_names
+    }
+    assert set(free_name_to_input.keys()) == free_names
+
+    # For each contracted dimension name, record the axis indices it occurs in 
+    # for each input
+    cnrt_name_to_input = {name: [] for name in list(cnrt_names)}
+    for ii_input, sig_input in enumerate(sig_inputs):
+        for ii_ax, name in enumerate(sig_input):
+            if name in cnrt_name_to_input:
+                cnrt_name_to_input[name].append(tuple([ii_input, ii_ax]))
+
+    return free_name_to_input, cnrt_name_to_input
