feed9465630ca0c04e62d99619ac79376df10501
Jonathan Deng <jonathan.j.deng@gmail.com>
2022-04-26 23:20:47 -0400

    Rename ufuncutils to ufunc

--- tests/test_ufuncutils.py
+++ tests/test_ufuncutils.py
@@ -1,12 +1,12 @@
 
 import numpy as np
-from blocktensor import ufuncutils, tensor as btensor
+from blocktensor import ufunc, tensor as btensor
 
 SIGNATURE = '(i,j),(j,k)->(i, k)'
 
 def test_parse_ufunc_signature():
     sig = SIGNATURE
-    input_sigs, output_sig = ufuncutils.parse_ufunc_signature(sig)
+    input_sigs, output_sig = ufunc.parse_ufunc_signature(sig)
 
     print(sig)
     print(input_sigs)
@@ -15,13 +15,13 @@ def test_parse_ufunc_signature():
 def test_interpret_ufunc_signature():
     sig_inputs = [('i', 'j'), ('j', 'k')]
     sig_outputs = [('i', 'k')]
-    print(ufuncutils.interpret_ufunc_signature(sig_inputs, sig_outputs))
+    print(ufunc.interpret_ufunc_signature(sig_inputs, sig_outputs))
 
 def test_split_shapes_by_signatures():
     shape_inputs = [(2, 3, 2, 4), (2, 3, 4, 2)]
     sig_inputs = [('i', 'j'), ('j', 'k')]
 
-    print(ufuncutils.split_shapes_by_signatures(shape_inputs, sig_inputs))
+    print(ufunc.split_shapes_by_signatures(shape_inputs, sig_inputs))
     # shape_outputs = []
 
 def test_calculate_output_shapes():
@@ -29,9 +29,9 @@ def test_calculate_output_shapes():
     sig_inputs = [('i', 'j'), ('j', 'k')]
     sig_outputs = [('i', 'k')]
 
-    ewise_input_shapes, core_input_shapes = ufuncutils.split_shapes_by_signatures(shape_inputs, sig_inputs)
+    ewise_input_shapes, core_input_shapes = ufunc.split_shapes_by_signatures(shape_inputs, sig_inputs)
 
-    ewise_output_shapes, core_output_shapes = ufuncutils.calculate_output_shapes(ewise_input_shapes, core_input_shapes, sig_inputs, sig_outputs)
+    ewise_output_shapes, core_output_shapes = ufunc.calculate_output_shapes(ewise_input_shapes, core_input_shapes, sig_inputs, sig_outputs)
     print(ewise_output_shapes, core_output_shapes)
 
 def test_gen_in_multi_index():
@@ -46,7 +46,7 @@ def test_gen_in_multi_index():
     ewise_input_ndims = [0, 0]
     out_midx = (9, 2)
 
-    gen_input_midx = ufuncutils.make_gen_in_multi_index(
+    gen_input_midx = ufunc.make_gen_in_multi_index(
         ewise_input_ndims, sig_inputs, sig_outputs[0]
     )
 
@@ -59,7 +59,7 @@ def test_recursive_concatenate():
     d = np.ones((2, 2))
     A = btensor.BlockTensor([[a, b], [c, d]])
 
-    ufuncutils.recursive_concatenate(A.subtensors_flat, A.shape, A.dims)
+    ufunc.recursive_concatenate(A.subtensors_flat, A.shape, A.dims)
 
 def test_apply_ufunc():
     a = np.ones((4, 4))
@@ -77,7 +77,7 @@ def test_apply_ufunc():
     # C = ufuncutils.apply_ufunc(np.add, '__call__', *[A, B])
     # print(C.shape)
 
-    D = ufuncutils.apply_ufunc(np.matmul, '__call__', *[A, B])
+    D = ufunc.apply_ufunc(np.matmul, '__call__', *[A, B])
     print(D.shape)
 
 
