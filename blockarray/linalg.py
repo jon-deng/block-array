7c9cffdaef518d58fb0e8a95af690c74d3ff441d
Jonathan Deng <jonathan.j.deng@gmail.com>
2022-07-07 19:35:03 -0400

    maint: Make linalg.py functions work with collapsed axes

--- blockarray/linalg.py
+++ blockarray/linalg.py
@@ -3,6 +3,8 @@ This module contains various linear algebra operations on block matrices/vectors
 """
 
 from functools import reduce
+import itertools
+from multiprocessing.sharedctypes import Value
 import typing
 
 from . import subops as gops
@@ -31,13 +33,17 @@ def mult_mat_vec(mat: bm.BlockMatrix[T], vec: bv.BlockVector[T]) -> bv.BlockVect
     bv.BlockVector
         The resulting block vector from the matrix-vector product
     """
+    ret_shape = (mat.f_shape[0],)
     ret_subvecs = []
-    for submat_row in mat:
+    # Uncollapse any blocks so that iteration over rows and columns is done
+    # The correct output shape (potentially collapsed), should be maintained 
+    # from `ret_shape`
+    for submat_row in mat.unsqueeze():
         ret_subvec = reduce(
             lambda a, b: a+b,
             [gops.mult_mat_vec(submat, subvec) for submat, subvec in zip(submat_row, vec)])
         ret_subvecs.append(ret_subvec)
-    return bv.BlockVector(ret_subvecs, labels=mat.f_labels[0:1])
+    return bv.BlockVector(ret_subvecs, shape=ret_shape, labels=mat.f_labels[0:1])
 
 def mult_mat_mat(mat_a: bm.BlockMatrix[T], mat_b: bm.BlockMatrix[T]) -> bm.BlockMatrix[T]:
     """
@@ -53,23 +59,26 @@ def mult_mat_mat(mat_a: bm.BlockMatrix[T], mat_b: bm.BlockMatrix[T]) -> bm.Block
     bm.BlockMatrix
         The resulting block matrix from the matrix-matrix product
     """
+    if mat_a.f_bshape[1] != mat_b.f_bshape[0]:
+        raise ValueError(
+            f"Matrices have incompatible block shapes {mat_a.f_bshape} and {mat_b.f_bshape}"
+        )
+
+    ret_shape = (mat_a.f_shape[0], mat_b.f_shape[1])
+    ret_labels = tuple([mat_a.f_labels[0], mat_b.f_labels[1]])
+
     ## ii/jj denote the current row/col indices
     NROW, NCOL = mat_a.f_shape[0], mat_b.f_shape[1]
 
     assert mat_a.f_bshape[1] == mat_b.f_bshape[0]
     NREDUCE = mat_a.f_shape[1]
 
-    mats = []
-    for ii in range(NROW):
-        mat_row = [
-            reduce(
-                lambda a, b: a + b,
-                [gops.mult_mat_mat(mat_a[ii, kk], mat_b[kk, jj]) for kk in range(NREDUCE)]
-            )
-            for jj in range(NCOL)
-        ]
-        mats.append(mat_row)
-
-    labels = tuple([mat_a.f_labels[0], mat_b.f_labels[1]])
-    return bm.BlockMatrix(mats, labels=labels)
+    ret_mats = [
+        reduce(
+            lambda a, b: a+b,
+            (gops.mult_mat_mat(mat_a[ii, kk], mat_b[kk, jj]) for kk in range(NREDUCE))
+        )
+        for ii, jj in itertools.product(range(NROW), range(NCOL))
+    ]
+    return bm.BlockMatrix(ret_mats, shape=ret_shape, labels=ret_labels)
 
