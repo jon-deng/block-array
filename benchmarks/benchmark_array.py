f96355f1f1ce6122e6ae8cfd12ebfa200d4582af
Jonathan Deng <jonathan.j.deng@gmail.com>
2022-08-11 11:31:17 -0400

    maint: Remove cProfile import in benchmark_array

--- benchmarks/benchmark_array.py
+++ benchmarks/benchmark_array.py
@@ -1,4 +1,9 @@
-import cProfile
+"""
+Contains functions for benchmarking `BlockArray` functions
+
+Run this using `cProfile`, `line_profiler`, etc. as an entry point to
+benchmarking
+"""
 
 import numpy as np
 
@@ -7,7 +12,9 @@ from blockarray.blockarray import BlockArray
 def setup_subarrays():
     sizes = (500, 500, 100, 9000)
     subarrays = [np.ones(size) for size in sizes]
-    return subarrays
+    _subarrays = np.empty(len(subarrays), dtype=object)
+    _subarrays[:] = subarrays
+    return _subarrays
 
 def benchmark_array_creation(subarrays):
     return BlockArray(subarrays, labels=(('a', 'b', 'c', 'd'),))
@@ -15,4 +22,5 @@ def benchmark_array_creation(subarrays):
 if __name__ == '__main__':
     subarrays = setup_subarrays()
 
-    cProfile.run('for n in range(2000): benchmark_array_creation(subarrays)', 'benchmark_array_creation.Profile')
+    for n in range(2000):
+        benchmark_array_creation(subarrays)
