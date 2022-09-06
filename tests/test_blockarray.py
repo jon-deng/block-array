5658c65f9141383935fa1cd2f2f4f29278c23e78
Jonathan Deng <jonathan.j.deng@gmail.com>
2022-09-05 23:58:49 -0400

    test: Refactor test_blockarray.py to use pytest idioms

--- tests/test_blockarray.py
+++ tests/test_blockarray.py
@@ -1,83 +1,97 @@
 """
-Test the operations in blockmath.py
+Test the operations in `blockarray.blockarray`
 """
+
+import operator
 
 import pytest
 import numpy as np
 
 import blockarray.blockarray as ba
 
+@pytest.fixture()
+def setup_barray_a():
+    a = np.ones((4, 4))
+    b = np.ones((4, 2))
+    c = np.ones((2, 4))
+    d = np.ones((2, 2))
+    return ba.BlockArray([[a, b], [c, d]]), (a, b, c, d)
 
-a = np.ones((4, 4))
-b = np.ones((4, 2))
-c = np.ones((2, 4))
-d = np.ones((2, 2))
-A = ba.BlockArray([[a, b], [c, d]])
+@pytest.fixture()
+def setup_barray_b():
+    a = np.ones((4, 4))
+    b = np.ones((4, 2))
+    c = np.ones((2, 4))
+    d = np.ones((2, 2))
+    return ba.BlockArray([[a, b], [c, d]]), (a, b, c, d)
 
-a = 2 * np.ones((4, 4))
-b = 2 * np.ones((4, 2))
-c = 2 * np.ones((2, 4))
-d = 2 * np.ones((2, 2))
-B = ba.BlockArray([[a, b], [c, d]])
+class TestBlockArray:
 
-def test_create_collapsed():
-    a = np.ones((2, 2))
-    A = ba.BlockArray([a], shape=(-1, -1))
-    assert (A is a)
+    def test_create_collapsed(self):
+        """
+        Test that creating reduced/collapsed blocks returns the subarray itself
+        """
+        a = np.ones((2, 2))
+        A = ba.BlockArray([a], shape=(-1, -1))
+        assert (A is a)
 
-def test_index():
-    assert np.all(B.sub[0, 0] == a)
-    assert np.all(B.sub[0, 1] == b)
-    assert np.all(B.sub[1, 0] == c)
-    assert np.all(B.sub[1, 1] == d)
+    def test_index(self, setup_barray_a):
+        B, (a, b, c, d) = setup_barray_a
+        assert np.all(B.sub[0, 0] == a)
+        assert np.all(B.sub[0, 1] == b)
+        assert np.all(B.sub[1, 0] == c)
+        assert np.all(B.sub[1, 1] == d)
 
-def _test_elementwise_binary_op(sub_op, a, b, block_op=None):
-    """
-    Test a generic element-wise binary operation
+    @pytest.fixture(
+        params=[
+            operator.add, operator.sub,
+            operator.mul, operator.truediv,
+            # operator.pow
+        ]
+    )
+    def binary_op(self, request):
+        """
+        Return a binary operation
+        """
+        return request.param
 
-    This should test whether a operation applied across each block gives the
-    same result as an equivalent operation on the BlockArrays
-    """
-    if block_op is None:
-        block_op = sub_op
-    c_array_result = block_op(a, b).sub[:].flat
-    c_array_reference = tuple(sub_op(ai, bi) for ai, bi in zip(a.sub[:].flat, b.sub[:].flat))
+    def test_elementwise_binary_op(self, binary_op, setup_barray_a, setup_barray_b):
+        """
+        Test a generic element-wise binary operation
 
-    correct_subarrays = [
-        np.all(sub_res == sub_ref)
-        for sub_res, sub_ref in zip(c_array_result, c_array_reference)
-    ]
+        This should test whether a operation applied across each block gives the
+        same result as an equivalent operation on the BlockArrays
+        """
+        a, *_ = setup_barray_a
+        b, *_ = setup_barray_b
 
-    assert all(correct_subarrays)
+        c_array_result = binary_op(a, b).sub[:].flat
+        c_array_reference = tuple(
+            binary_op(ai, bi) for ai, bi in zip(a.sub[:].flat, b.sub[:].flat)
+        )
 
-def test_add():
-    _test_elementwise_binary_op(lambda x, y: x+y, A, B)
+        correct_subarrays = [
+            np.all(sub_res == sub_ref)
+            for sub_res, sub_ref in zip(c_array_result, c_array_reference)
+        ]
 
-def test_sub():
-    _test_elementwise_binary_op(lambda x, y: x-y, A, B)
+        assert all(correct_subarrays)
 
-def test_mul():
-    _test_elementwise_binary_op(lambda x, y: x*y, A, B)
+    def test_bshape(self, setup_barray_a):
+        A, *_ = setup_barray_a
+        assert A.f_bshape == ((4, 2), (4, 2))
+        print(f"A.bshape = {A.f_bshape}")
+        print(f"A[:, :].bshape = {A[:, :].f_bshape}")
+        print(f"A[:, 0].bshape = {A[:, 0].f_bshape}")
+        print(f"A[0, :].bshape = {A[0, :].f_bshape}")
 
-def test_div():
-    _test_elementwise_binary_op(lambda x, y: x/y, A, B)
+    def test_squeeze(self, setup_barray_a):
+        A, *_ = setup_barray_a
+        dd = A[0, :]
+        assert dd.unsqueeze().f_bshape == ((4,), (4, 2))
 
-def test_power():
-    _test_elementwise_binary_op(lambda x, y: x**y, A, B, ba.power)
-
-def test_bshape():
-    assert A.f_bshape == ((4, 2), (4, 2))
-    print(f"A.bshape = {A.f_bshape}")
-    print(f"A[:, :].bshape = {A[:, :].f_bshape}")
-    print(f"A[:, 0].bshape = {A[:, 0].f_bshape}")
-    print(f"A[0, :].bshape = {A[0, :].f_bshape}")
-
-def test_squeeze():
-    dd = A[0, :]
-    assert dd.unsqueeze().f_bshape == ((4,), (4, 2))
-
-    dd = A[0:1, :]
-    assert dd.squeeze().f_bshape == (4, (4, 2))
+        dd = A[0:1, :]
+        assert dd.squeeze().f_bshape == (4, (4, 2))
 
 # TODO: This isn't the right way to parameterize a test function
 @pytest.fixture(params=[
@@ -89,7 +103,8 @@ def test_ones(request):
     A = ba.zeros(request.param)
     assert A.f_bshape == request.param
 
-def test_ufunc():
+def test_ufunc(setup_barray_a):
+    A, *_ = setup_barray_a
     for op in [np.add, np.multiply, np.divide]:
         D = op(5.0, A)
         _D = op(5.0, A.to_mono_ndarray())
